b8e71b2c557997ac5bf2a89a36c687332cc1229a
ercüment <41874858+ercumentlacin@users.noreply.github.com>
2021-01-09 10:33:52 +0300

    Update AppContainer.jsx

--- src/Components/AppContainer.jsx
+++ src/Components/AppContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 
